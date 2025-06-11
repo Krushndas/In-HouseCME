3dbf0e6a3c4fec1a01d525b56272065666c4e94c
krushndas <kchaudhari.techark@gmail.com>
2025-06-11 12:22:05 +0530

    Added maven files.

--- src/main/java/com/CME/BaseClass.java
+++ src/main/java/com/CME/BaseClass.java
@@ -9,7 +9,7 @@ import static com.core_automation.base.BaseTest.fs;
 
 public class BaseClass {
     public static void setup(){
-        BaseTest.setUp("web", "src"+fs+"main"+fs+"resources"+fs+"config.yml", true);
+        BaseTest.setUp("web", "src"+fs+"main"+fs+"resources"+fs+"config.yml", false);
         ConfigReader.setConfig("src"+fs+"main"+fs+"resources"+fs+"config.yml");
         new TestDataUtil("src"+fs+"main"+fs+"java"+fs+"com"+fs+"CME"+fs+"TestData"+fs+"data.json");
         new LocatorUtil("src"+fs+"test"+fs+"resources"+fs+"ObjectRepositories"+fs+"locators.properties");
